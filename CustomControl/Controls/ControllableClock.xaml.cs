cd5846e972e3408a8d6a3f166ef426f56052da17
Willem Boshoff <willem.boshoff@dyna-mo.com>
2021-05-26 09:23:05 +0200

    Show and change multiple clocks using composition instead of inheritance.

---
+++ CustomControl/Controls/ControllableClock.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CustomControl.Controls
+{
+    public partial class ControllableClock : UserControl
+    {
+        public static readonly DependencyProperty ClockProperty = 
+            DependencyProperty.Register("Clock", typeof(Clock), typeof(ControllableClock), new PropertyMetadata(null));
+
+        public Clock Clock
+        {
+            get => (Clock) GetValue(ClockProperty);
+            set => SetValue(ClockProperty, value);
+        }
+        
+        public ControllableClock()
+        {
+            InitializeComponent();
+        }
+    }
+}
